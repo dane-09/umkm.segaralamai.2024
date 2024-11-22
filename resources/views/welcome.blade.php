4cab75bc6b163b4daf30a81c0cabb92136468461
ainun <ainun23ti@mahasiswa.pcr.ac.id>
2024-11-22 10:55:09 +0700

    Mengubah Judul 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>UMKM UMKM</title>
+        <title>UMKM UMKM UMKM</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
